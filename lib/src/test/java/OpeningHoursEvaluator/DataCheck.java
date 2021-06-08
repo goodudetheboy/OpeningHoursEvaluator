ab8c3881a1d23c73253e8f610a0d9e0779d64bbe
Vuong Ho <hochivuong2002@gmail.com>
2021-06-08 21:36:00 +0700

    Add input time checking against parser

---
+++ lib/src/test/java/OpeningHoursEvaluator/DataCheck.java
@@ -0,0 +1,63 @@
+package openinghoursevaluator;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import org.junit.Test;
+
+import ch.poole.openinghoursparser.OpeningHoursParser;
+import ch.poole.openinghoursparser.ParseException;
+
+public class DataCheck {
+    @Test
+    public void inputTimeFolderLegalTest() {
+        File[] files = new File("test-data\\input-time").listFiles();
+        for(File file : files) {
+            assertTrue(inputTimeFileLegalTest(file.getPath(), false));
+        }
+        // fail("Very bad");
+    }   
+
+    public boolean inputTimeFileLegalTest(String inputFileDir, boolean isStrict) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFileDir), StandardCharsets.UTF_8));
+            String data;
+            int lineNum = 1;
+            while ((data = reader.readLine()) != null) {
+                try {
+                    OpeningHoursParser parser = new OpeningHoursParser(new  ByteArrayInputStream(data.getBytes()));
+                    List<ch.poole.openinghoursparser.Rule> rules = parser.rules(isStrict);
+                } catch (ParseException pex) {
+                    fail("Input value \"" + data + "\" is illegal in line " + lineNum + " of file " + inputFileDir);
+                    return false;
+                }
+                lineNum++;
+            } 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            return false;
+        } finally {
+            try { 
+                reader.close();
+            } catch (IOException ioe) {
+                fail("Error closing BufferedReader");
+                return false;
+            }
+        }
+        return true;
+    }
+}

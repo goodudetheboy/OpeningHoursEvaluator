33b5905fa66bc17a9464c1d6d0e34292bc334c2a
Vuong Ho <hochivuong2002@gmail.com>
2021-06-10 22:00:11 +0700

    Fix timepoint checking

--- lib/src/main/java/openinghoursevaluator/OpeningHoursEvaluator.java
+++ lib/src/main/java/openinghoursevaluator/OpeningHoursEvaluator.java
@@ -22,7 +22,6 @@ public class OpeningHoursEvaluator {
 
     /** Constructor with input time string according to opening hours specification */
     public OpeningHoursEvaluator(String openingHours, boolean isStrict) {
-        
         OpeningHoursParser parser = new OpeningHoursParser(new ByteArrayInputStream(openingHours.getBytes()));
         try {
             rules = parser.rules(isStrict);
@@ -41,32 +40,50 @@ public class OpeningHoursEvaluator {
      */
     public boolean checkStatus(String inputTime) {
         LocalDateTime time = LocalDateTime.parse(inputTime);
+        Rule ruleGood = null;
         // to be expanded later for checking weekday, day, month, year
-        if(!checkStatusWithTimePoint(timeInMinute(time))) {
-            return false;
+        for(Rule rule : rules) {
+            if(ruleGood != null && checkSameDay(ruleGood, rule, time)) ruleGood = null;
+            if(ruleGood == null && checkStatusWithTimePoint(timeInMinute(time), rule)) ruleGood = rule;
         }
-        return true;
+        return ruleGood != null;
     }
 
-    public boolean checkStatusWithTimePoint(int timepoint) {
-        for(Rule rule : rules) {
-            if(rule.getTimes() != null) {
-                List<TimeSpan> times = rule.getTimes();
-                for(TimeSpan time : times) {
-                    if(time.getEnd() == Integer.MIN_VALUE) {
-                        if(timepoint == time.getStart()) {
-                            return true;
-                        }
-                    } else if(timepoint >= time.getStart() && timepoint <= time.getEnd()) {
-                            return true;
-                    }
-                }
+    /**
+     * Check if avenue is closed or not according to an input time string in accordance with
+     * LocalDateTime parser with respect to a Rule and a timepoint
+     * 
+     * @param inputpoint input timepoint in minutes form
+     * @param rule input rule
+     */
+    public boolean checkStatusWithTimePoint(int timepoint, Rule rule) {
+        if(rule.getTimes() != null) {
+            List<TimeSpan> times = rule.getTimes();
+            for(TimeSpan time : times) {
+                if(time.getEnd() == Integer.MIN_VALUE && timepoint == time.getStart()) return true;
+                else if(timepoint >= time.getStart() && timepoint <= time.getEnd()) return true;
             }
+        } else {
+            return true;
         }
-        // Return false if no TimeSpan rules contains timepoint
+        // Return false if no timespan in input Rule contains timepoint
         return false;
     }
 
+    /**
+     * Check if two input rules are affecting the same day
+     * 
+     * @param rule1 rule 1
+     * @param rule2 rule 2
+     */
+    public boolean checkSameDay(Rule rule1, Rule rule2, LocalDateTime time) {
+        return rule1.getDays() == null && rule2.getDays() == null;
+    }
+
+    /** Convert hour and minute of a LocalDateTime instance to minutes 
+     *  
+     * @param time a LocalDateTime instance
+    */
     public int timeInMinute(LocalDateTime time) {
         return time.getHour()*60 + time.getMinute();
     }

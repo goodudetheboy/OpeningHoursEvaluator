797847a88cba1f71a6c3990c170651c0bb5e6baa
Vuong Ho <hochivuong2002@gmail.com>
2021-07-01 10:13:34 +0700

    Add partial support for easter

--- lib/src/main/java/openinghoursevaluator/DateManager.java
+++ lib/src/main/java/openinghoursevaluator/DateManager.java
@@ -4,9 +4,12 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
+
 import ch.poole.openinghoursparser.DateRange;
 import ch.poole.openinghoursparser.DateWithOffset;
 import ch.poole.openinghoursparser.Month;
+import ch.poole.openinghoursparser.VarDate;
 import ch.poole.openinghoursparser.WeekDay;
 import ch.poole.openinghoursparser.WeekDayRange;
 import ch.poole.openinghoursparser.YearRange;
@@ -35,7 +38,7 @@ public class DateManager {
         List<List<LocalDate>> result = new ArrayList<>();
         DateWithOffset start = dateRange.getStartDate();
         DateWithOffset end = dateRange.getEndDate();
-        checkError(start, end);
+        checkError(start, end, defaultYear);
         // there is always a start of DateRange
         List<LocalDate> subResult = new ArrayList<>();
         result.add(subResult);
@@ -44,8 +47,9 @@ public class DateManager {
             // handle when there is no year specified but there is year wrapping
             // the compare below only check for date and month
             // @see https://wiki.openstreetmap.org/wiki/Key:opening_hours/specification#explain:monthday_range:date_offset:to:date_offset
-            if (compareStartAndEnd(start, end) > 0) {
+            if (compareStartAndEnd(start, end, defaultYear) > 0) {
                 subResult.add(DateManager.convertToLocalDate(end, defaultYear + 1, start.getMonth()));
+                // get year wraping from previous year
                 List<LocalDate> otherResult = new ArrayList<>();
                 otherResult.add(DateManager.convertToLocalDate(start, defaultYear-1, start.getMonth()));
                 otherResult.add(DateManager.convertToLocalDate(end, defaultYear, start.getMonth()));
@@ -62,15 +66,50 @@ public class DateManager {
      * 
      * @param start start of a DateRange
      * @param end end of a DateRange
+     * @param easterYear optional year, used if there's easter in one of the ends
      * @return <0 if d1 is before d2, >0 if d1 is after d2, =0 if d1 is same day as d2
      */
-    private int compareStartAndEnd(DateWithOffset start, DateWithOffset end) {
+    private int compareStartAndEnd(DateWithOffset start, DateWithOffset end, int easterYear) {
+        if (isEaster(start)) {
+            int year = (start.getYear() != YearRange.UNDEFINED_YEAR)
+                            ? start.getYear()
+                            : easterYear;
+            fillEaster(start, year);
+        }
+        if (isEaster(end)) {
+            int year = (end.getYear() != YearRange.UNDEFINED_YEAR)
+                            ? end.getYear()
+                            : easterYear;
+            fillEaster(end, year);
+        }
         return ((end.getMonth() == null) || start.getMonth() == end.getMonth())
                 ? start.getDay() - end.getDay()
                 : start.getMonth().ordinal() - end.getMonth().ordinal();
     }
 
-    public void checkError(DateWithOffset start, DateWithOffset end) {
+    /**
+     * Helper function for compareStartAndEnd().
+     * <p>
+     * Populate input DateWithOffset with month and day off easter, for 
+     * checking day and month in compareStartAndEnd().
+     * 
+     * */
+    private void fillEaster(DateWithOffset date, int easterYear) {
+        LocalDate easter = getEasterDate(easterYear);
+        date.setDay(easter.getDayOfMonth());
+        date.setMonth(MonthRule.convertMonth(easter));
+    }
+
+    /**
+     * Error checking between a start date and an end date of DateRange.
+     * <p>
+     * Failing condition TBU.
+     * 
+     * @param start start DateWithOffset of a DateRange
+     * @param end end DateWithOffset of a DateRange
+     * @param easterYear optional easter year, TODO: temporary, needs removing
+     */
+    public void checkError(DateWithOffset start, DateWithOffset end, int easterYear) {
         String rangeString = "(" + start + " - " + end + ")";
         if (end == null) {
             return;
@@ -83,13 +122,22 @@ public class DateManager {
         }
         // check if start is after end
         if (start.getYear() != YearRange.UNDEFINED_YEAR
-                && ((compareStartAndEnd(start, end) > 0)
+                && ((compareStartAndEnd(start, end, easterYear) > 0)
                         || (end.getYear() != YearRange.UNDEFINED_YEAR
                             && start.getYear() > end.getYear()))) {
             throw new IllegalArgumentException("Illegal range " + rangeString + ", please double check");
         }
     }
 
+    /**
+     * Check if input DateWithOffset is an Easter day
+     * 
+     * @param date input DateWithOffset
+     * @return if its variable date is easter
+     */
+    public static boolean isEaster(DateWithOffset date) {
+        return date.getVarDate() != null && date.getVarDate() == VarDate.EASTER;
+    }
 
     /**
      * Special conversion of DateWithOffset to date, with optionalYear and
@@ -106,23 +154,62 @@ public class DateManager {
      * @return a corresponding LocalDateTime
      */
     public static LocalDate convertToLocalDate(DateWithOffset date, int optionalYear, Month optionalMonth) {
-        int monthInt = (date.getMonth() == null)
-                        ? optionalMonth.ordinal() + 1
-                        : date.getMonth().ordinal() + 1;
         int yearInt = (date.getYear() == YearRange.UNDEFINED_YEAR)
                         ? optionalYear
                         : date.getYear();
-        int offset = date.getDayOffset();
-        LocalDate pending = LocalDate.of(yearInt, monthInt, date.getDay());
+        LocalDate pending = null;
+        // check for variable date
+        if (date.getVarDate() != null) {
+            // to be updated when more variable date is added
+            if (date.getVarDate() == VarDate.EASTER) {
+                pending = getEasterDate(yearInt);
+            }
+        } else {
+            int monthInt = (date.getMonth() == null)
+                                ? optionalMonth.ordinal() + 1
+                                : date.getMonth().ordinal() + 1;
+            pending = LocalDate.of(yearInt, monthInt, date.getDay());
+        }
         // handle weekday offset
-        if (date.getWeekDayOffset() != null) {
+        if (date.getWeekDayOffset() != null && pending != null) {
             pending = findNextWeekDay(pending, date.isWeekDayOffsetPositive(),
                                         date.getWeekDayOffset());
         }
         // handle day offset
-        return getOffsetDate(pending, offset);
+        return getOffsetDate(pending, date.getDayOffset());
     }
     
+    /**
+     * Returns the easter day of an input year.
+     * <p>
+     * This function uses the  "Meeus/Jones/Butcher" algorithm. For more
+     * information check the link provided below.
+     * 
+     * @param year valid Gregorian year
+     * @return the date of Easter of input year
+     * @author Bernhard Seebass, from https://stackoverflow.com/a/55278990/10154717
+     * @see "Meeus/Jones/Butcher" algorithm
+     * https://en.wikipedia.org/wiki/Date_of_Easter#Anonymous_Gregorian_algorithm
+     */
+    public static LocalDate getEasterDate(int year) {
+        int a = year % 19;
+        int b = year / 100;
+        int c = year % 100;
+        int d = b / 4;
+        int e = b % 4;
+        int f = (b + 8) / 25;
+        int g = (b - f + 1) / 3;
+        int h = (19 * a + b - d - g + 15) % 30;
+        int i = c / 4;
+        int k = c % 4;
+        int l = (32 + 2 * e + 2 * i - h - k) % 7;
+        int m = (a + 11 * h + 22 * l) / 451;
+        int easterMonth = (h + l - 7 * m + 114) / 31;
+        int p = (h + l - 7 * m + 114) % 31;
+        int easterDay = p + 1;
+        return LocalDate.of(year, easterMonth, easterDay);
+    }
+
     /**
      * Return the date that is offset by some days from the input date,
      * according to the following rule:
@@ -156,7 +243,8 @@ public class DateManager {
      * @param weekday
      * @return
      */
-    public static LocalDate findNextWeekDay(LocalDate date, boolean isAfter, WeekDay weekday) {
+    @NonNull
+    public static LocalDate findNextWeekDay(@NonNull LocalDate date, boolean isAfter, WeekDay weekday) {
         if (Week.convertWeekDay(date.getDayOfWeek()) == weekday) {
             return date;
         }
